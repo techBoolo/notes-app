cf5725c5c9dc18d87668c76ef32b562bbcc1c878
teklu <teklu@duck.com>
2022-11-23 16:41:10 +0300

    receiving data

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 import express from 'express'
-
-const app = express()
+import { randomBytes } from 'crypto'
 import { notesData } from './data/notes.js'
+
+const generateId = () => randomBytes(2).toString('hex')
+const app = express()
+
 let notes = [ ...notesData ]
+app.use(express.json())
 
 app.get('/', (req, res) => {
   res.send('it works')
@@ -14,7 +18,7 @@ app.get('/notes', (req, res) => {
 
 app.get('/notes/:id', (req, res) => {
   const { id } = req.params
-  const note = notes.find(n => n.id === Number(id))
+  const note = notes.find(n => n.id === id)
   if(note) {
     res.status(200).json(note)
   } else {
@@ -25,9 +29,27 @@ app.get('/notes/:id', (req, res) => {
 
 app.delete('/notes/:id', (req, res) => {
   const { id } = req.params
-  notes = notes.filter(note => note.id !== Number(id))
+  notes = notes.filter(note => note.id !== id)
   res.statusCode = 204
   res.end()
+})
+
+app.post('/notes', (req, res) => {
+  const body = req.body
+  if(!body.content) {
+    return res.status(400).json({
+      error: 'content missing'
+    })
+  }
+  const id = generateId()
+  const note = { 
+    id, 
+    content: body.content,
+    date: new Date(),
+    important: body.important || Math.random() > 0.5 ? true : false
+  }
+  notes.push(note)
+  res.status(201).json(note)
 })
 
 export default app
