9d030dc1554583f8d96c4143c42b9d34095645fe
teklu <teklu@duck.com>
2022-11-23 14:58:17 +0300

    serve list of notes

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 import http from 'http'
 
 const PORT = process.env.PORT || 3001
+import { notes } from './data/notes.js'
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' })
-  res.end('Hello\n')
+  res.writeHead(200, { 'Content-Type': 'application/json' })
+  res.end(JSON.stringify(notes))
 })
 
 server.listen(PORT, () => {
