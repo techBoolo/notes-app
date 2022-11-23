a28c1dd518cef102327ab134e2200ed5357f0c7b
teklu <teklu@duck.com>
2022-11-23 14:53:40 +0300

    create a server

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
-console.log('hello');
+import http from 'http'
+
+const PORT = process.env.PORT || 3001
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' })
+  res.end('Hello\n')
+})
+
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+})
