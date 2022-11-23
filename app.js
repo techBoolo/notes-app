c960c33ed1c8e195abd2cbd8afe98de4c91d8fa5
teklu <teklu@duck.com>
2022-11-23 15:46:30 +0300

    fetch a resource, delete a resource

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 import express from 'express'
 
 const app = express()
-import { notes } from './data/notes.js'
+import { notesData } from './data/notes.js'
+let notes = [ ...notesData ]
 
 app.get('/', (req, res) => {
   res.send('it works')
@@ -9,6 +10,24 @@ app.get('/', (req, res) => {
 
 app.get('/notes', (req, res) => {
   res.status(200).json(notes)
+})
+
+app.get('/notes/:id', (req, res) => {
+  const { id } = req.params
+  const note = notes.find(n => n.id === Number(id))
+  if(note) {
+    res.status(200).json(note)
+  } else {
+    res.statusMessage = "Note can't find" 
+    res.status(404).end()
+  }
+})
+
+app.delete('/notes/:id', (req, res) => {
+  const { id } = req.params
+  notes = notes.filter(note => note.id !== Number(id))
+  res.statusCode = 204
+  res.end()
 })
 
 export default app
