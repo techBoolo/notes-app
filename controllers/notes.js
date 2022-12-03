cdbe5b3b2b6ef6c96af7c9c3750dfccadaae6435
teklu <teklu@duck.com>
2022-12-03 17:17:09 +0300

    eliminate try/catch in the controller

--- controllers/notes.js
+++ controllers/notes.js
@@ -1,5 +1,6 @@
 import { getNotes, getNote, createNote, deleteNote, updateNoteImportance } from '../db/model.js'
 import ErrorResponse from '../utils/errorResponse.js'
+import('express-async-errors')
 
 const index = async (req, res) => {
   const notes = await getNotes()
@@ -7,10 +8,9 @@ const index = async (req, res) => {
 }
 
 const create = async (req, res, next) => {
-  try {
     const body = req.body
     if(!body.content) {
-      throw new ErrorResponse(400, 'content missingg')
+       return next(new ErrorResponse(400, 'content missingg'))
     }
     const userData = { 
       content: body.content,
@@ -19,57 +19,38 @@ const create = async (req, res, next) => {
     }
     const note = await createNote(userData) 
     res.status(201).json(note)
-  } catch (err) {
-    const error = new ErrorResponse(err.statusCode, err.message)
-    next(error)
-  }
 }
 
 const show = async (req, res, next) => {
-  try {
     const { id } = req.params
     const note = await getNote(id)
     if(note) {
       res.status(200).json(note)
     } else {
-      throw new ErrorResponse(404, "Note can't find")
+      next(new ErrorResponse(404, "Note can't find"))
     }
-  } catch (err) {
-    const error = new ErrorResponse(err.statusCode, err.message)
-    next(error)
-  }
 }
 
-const remove = async (req, res) => {
-  try {
+const remove = async (req, res, next) => {
     const { id } = req.params
     const response = await deleteNote(id)
     if(response) {
       res.statusCode = 204
       res.end()
     } else {
-      res.status(400).json({ message: 'note not found.' })
+      next(new ErrorResponse(404, 'note not found'))
     }
-  } catch (err) {
-    const error = new ErrorResponse(400, err.message)
-    next(error)
-  }
 }
 
 const update = async (req, res, next) => {
   const body = req.body
   const id = req.params.id
-  try {
     const response = await updateNoteImportance(id, body.important)
     if(response) {
-      return res.status(200).json(response)
+      res.status(200).json(response)
     } else {
-      throw new ErrorResponse(404, "Note can't found.")
+      next(new ErrorResponse(404, "Note can't found."))
     }
-  } catch (err) {
-    const error = new ErrorResponse(err.statusCode, err.message)
-    next(error)
-  }
 }
 
 export default {
